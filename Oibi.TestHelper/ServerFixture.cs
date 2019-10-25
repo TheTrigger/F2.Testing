4a490915c2dea555b166d22a3c7c79f1eb4cd144
Fabio <f.fabrizi91@gmail.com>
2019-10-25 14:14:46 +0200

    updated

--- Oibi.TestHelper/ServerFixture.cs
+++ Oibi.TestHelper/ServerFixture.cs
@@ -7,58 +7,57 @@ using System.Net.Http;
 
 namespace Oibi.TestHelper
 {
-    public class ServerFixture<TStartup> : IDisposable where TStartup : class
-    {
-        /// <summary>
-        /// Current <see cref="TStartup"/> server
-        /// </summary>
-        public TestServer Server { get; private set; }
+	public class ServerFixture<TStartup> : IDisposable where TStartup : class
+	{
+		/// <summary>
+		/// Current <see cref="TStartup"/> server
+		/// </summary>
+		public TestServer Server { get; private set; }
 
-        /// <summary>
-        /// Your <see cref="HttpClient"/>
-        /// </summary>
-        public HttpClient Client { get; private set; }
+		/// <summary>
+		/// Your <see cref="HttpClient"/>
+		/// </summary>
+		public HttpClient Client { get; private set; }
 
-        /// <summary>
-        /// Get service by type
-        /// </summary>
-        public TService GetService<TService>() => (TService)Server.Host.Services.GetService(typeof(TService));
+		/// <summary>
+		/// Get service by type
+		/// </summary>
+		public TService GetService<TService>() => (TService)Server.Host.Services.GetService(typeof(TService));
 
-        public ICollection GetRoutesOfController() => throw new NotImplementedException();
-        public ICollection GetRoutesOfControllerMethod() => throw new NotImplementedException();
+		//public ICollection GetRoutesOfController() => throw new NotImplementedException();
+		//public ICollection GetRoutesOfControllerMethod() => throw new NotImplementedException();
 
-        /// <summary>
-        /// Instantiate a test server with asppsettings or environment valiables
-        /// </summary>
-        public ServerFixture()
-        {
-            IWebHostBuilder builder = new WebHostBuilder().UseEnvironment("Development")
-                    .ConfigureAppConfiguration((hostingContext, config) =>
-                    {
-                        config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-                        config.AddJsonFile("appsettings.test.json", optional: true, reloadOnChange: true);
-                        config.AddEnvironmentVariables();
-                    })
-                    .UseStartup<TStartup>();
+		/// <summary>
+		/// Instantiate a test server with asppsettings or environment valiables
+		/// </summary>
+		public ServerFixture()
+		{
+			IWebHostBuilder builder = new WebHostBuilder().UseEnvironment("Development")
+					.ConfigureAppConfiguration((hostingContext, config) =>
+					{
+						config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+						config.AddJsonFile("appsettings.test.json", optional: true, reloadOnChange: true);
+						config.AddEnvironmentVariables();
+					})
+					.UseStartup<TStartup>();
 
-            // only 1 build allowed WebHost = builder.Build();
-            Server = new TestServer(builder);
-            Client = Server.CreateClient();
-        }
+			Server = new TestServer(builder);
+			Client = Server.CreateClient();
+		}
 
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
 
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                Client?.Dispose();
-                Server?.Dispose();
-            }
-        }
-    }
+		protected virtual void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				Client?.Dispose();
+				Server?.Dispose();
+			}
+		}
+	}
 }
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # TestHelper
 
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/643b3b1a38644b0dab7f6d160bd08ec8)](https://www.codacy.com/manual/TheTrigger/TestHelper?utm_source=github.com&utm_medium=referral&utm_content=TheTrigger/TestHelper&utm_campaign=Badge_Grade)
+
 Don't be scared by tests 😨
 
 ```CSharp
