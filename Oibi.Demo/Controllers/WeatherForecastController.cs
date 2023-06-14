93e0adbf0a77fa1d6689771e18d651ece8e90f17
Fabio Fabrizi <f.fabrizi91@gmail.com>
2023-06-14 17:28:21 +0200

    removed async

--- Oibi.Demo/Controllers/WeatherForecastController.cs
+++ Oibi.Demo/Controllers/WeatherForecastController.cs
@@ -7,60 +7,60 @@ using System.Threading.Tasks;
 
 namespace Oibi.Demo.Controllers
 {
-	[ApiController]
-	[Route("[controller]")]
-	public class WeatherForecastController : ControllerBase
-	{
-		private static readonly string[] Summaries = new[]
-		{
-			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-		};
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : Controller
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
-		private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<WeatherForecastController> _logger;
 
-		public WeatherForecastController(ILogger<WeatherForecastController> logger)
-		{
-			_logger = logger;
-		}
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
 
-		[HttpGet]
-		public async Task<IActionResult> OnGet()
-		{
-			var rng = new Random();
-			return Ok(Enumerable.Range(1, 5).Select(index => new WeatherForecast
-			{
-				Date = DateTime.Now.AddDays(index),
-				TemperatureC = rng.Next(-20, 55),
-				Summary = Summaries[rng.Next(Summaries.Length)]
-			}));
-		}
-
-
-		[HttpPost]
-		public async Task<IActionResult> OnPost([FromBody] DataExample data)
-		{
-			return Ok(data);
-		}
-
-		[HttpPut]
-		public async Task<IActionResult> OnPut([FromBody] DataExample data)
-		{
-			return Ok(data);
-		}
-
-		[HttpDelete]
-		public async Task<IActionResult> OnDelete()
-		{
-			return Ok(new { Ok = true });
-		}
+        [HttpGet]
+        public IActionResult OnGet()
+        {
+            var rng = new Random();
+            return Json(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }));
+        }
 
 
-		public class DataExample
-		{
-			[Required]
-			public string Name { get; set; }
-			public int Age { get; set; }
-			public DateTime BirthDay { get; set; }
-		}
-	}
+        [HttpPost]
+        public IActionResult OnPost([FromBody] DataExample data)
+        {
+            return Ok(data);
+        }
+
+        [HttpPut]
+        public IActionResult OnPut([FromBody] DataExample data)
+        {
+            return Ok(data);
+        }
+
+        [HttpDelete]
+        public IActionResult OnDelete()
+        {
+            return Ok(new { Ok = true });
+        }
+
+
+        public class DataExample
+        {
+            [Required]
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public DateTime BirthDay { get; set; }
+        }
+    }
 }
